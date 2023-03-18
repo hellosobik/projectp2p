28e0b7bb176ac1f099ab5c9886a037937bba5867
Sobik <hellosobik@gmail.com>
2023-03-18 13:37:00 +0530

    created combined sockets file

---
+++ com_soc.py
@@ -0,0 +1,26 @@
+#combining client and server script!
+"""
+# This is client.py file
+import socket, base64               # Import socket module
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.connect((host, port))
+print(s.recv(1024).decode("utf-8"))
+s.close()                     # Close the socket when done
+"""
+"""
+# This is server.py file
+import socket, base64               # Import socket module
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print('Got connection from', addr)
+   c.send('Thank you for connecting'.encode("utf-8"))
+   c.close()                # Close the connection
+"""
+
